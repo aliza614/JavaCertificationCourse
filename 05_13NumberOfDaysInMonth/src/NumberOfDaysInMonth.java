2ff7f1995f79c86596fc7a6af5e4ce78edd0ea3e
Aliza <alizaoconnor1@yahoo.com>
2022-01-23 16:54:28 -0600

    take 3

--- 05_13NumberOfDaysInMonth/src/NumberOfDaysInMonth.java
+++ 05_13NumberOfDaysInMonth/src/NumberOfDaysInMonth.java
@@ -1,6 +1,6 @@
 public class NumberOfDaysInMonth {
     public static boolean isLeapYear(int year){
-         return (year>0)&&((year%4==0&&year%100!=0)||(year%400==0));
+         return (year>0)&&(year<=9999)&&((year%4==0&&year%100!=0)||(year%400==0));
     }
     public static int getDaysinMonth (int month, int year){
         if (month<1||month>12||year<1||year>9999)
