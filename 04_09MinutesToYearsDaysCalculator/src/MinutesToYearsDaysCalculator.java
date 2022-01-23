cdadfc28d22937048854bbc5faddedc18a630788
Aliza <alizaoconnor1@yahoo.com>
2022-01-23 15:42:31 -0600

    take 2

--- 04_09MinutesToYearsDaysCalculator/src/MinutesToYearsDaysCalculator.java
+++ 04_09MinutesToYearsDaysCalculator/src/MinutesToYearsDaysCalculator.java
@@ -1,9 +1,13 @@
 public class MinutesToYearsDaysCalculator {
     public static void printYearsAndDays (long minutes)
     {
-        long daysTotal=minutes/60/24;
-        long years=(long)(daysTotal/365.25);
-        long days=(long)(daysTotal%365.25);
-        System.out.println(minutes+" min = "+years+" y and "+days+" d");
+        if (minutes<0) {
+            System.out.println("Invalid Value");
+        }else{
+            long daysTotal=minutes/60/24;
+            long years=(long)(daysTotal/365.25);
+            long days=(long)(daysTotal%365.25);
+            System.out.println(minutes+" min = "+years+" y and "+days+" d");
+        }
     }
 }
