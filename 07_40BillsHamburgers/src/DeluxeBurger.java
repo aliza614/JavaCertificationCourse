8ea885dbc0c937849cfa78f9f532663e326bcd2b
Aliza <alizaoconnor1@yahoo.com>
2022-01-28 13:07:27 -0700

    take 2

--- 07_40BillsHamburgers/src/DeluxeBurger.java
+++ 07_40BillsHamburgers/src/DeluxeBurger.java
@@ -1,2 +1,36 @@
-public class DeluxeBurger {
+public class DeluxeBurger extends Hamburger{
+    public DeluxeBurger() {
+        super("Deluxe", "sausage", 19.10, "white");
+    }
+
+    @Override
+    public void addHamburgerAddition1(String name, double price) {
+        System.out.println("No additional items can ba added to a deluze burger.");
+    }
+
+    @Override
+    public void addHamburgerAddition2(String name, double price) {
+        System.out.println("No additional items can ba added to a deluze burger.");
+    }
+
+    @Override
+    public void addHamburgerAddition3(String name, double price) {
+        System.out.println("Cannot not add additional items to a deluxe burger");
+    }
+
+    @Override
+    public void addHamburgerAddition4(String name, double price) {
+        System.out.println("Cannot not add additional items to a deluxe burger");
+    }
+
+
+
+    public double itemizeHamburger() {
+        System.out.println( "Deluxe hamburger on a "+getBreadRollType()+" roll with "+super.getMeat()+", price is "+(super.getPrice()-2.75-1.81));
+        System.out.println( "Added Chips for an extra 2.75\n" +
+                "    Added Drink for an extra 1.81");
+
+        System.out.println( "Total Deluxe Burger price is  "+super.getPrice());
+        return super.getPrice();
+    }
 }
--- 07_40BillsHamburgers/src/HealthyBurger.java
+++ 07_40BillsHamburgers/src/HealthyBurger.java
@@ -1,2 +1,30 @@
-public class HealthyBurger {
+public class HealthyBurger extends Hamburger {
+    private String healthyExtra1Name;
+    private double healthyExtra1Price;
+    private String healthyExtra2Name;
+    private double healthyExtra2Price;
+
+    public HealthyBurger(String meat, double price) {
+        super("healthy", meat, price, "brown rye");
+    }
+
+
+    public void addHealthyAddition1(String name, double price) {
+        this.healthyExtra1Name=name;
+        this.healthyExtra1Price=price;
+    }
+    public void addHealthyAddition2(String name, double price) {
+        this.healthyExtra2Name=name;
+        this.healthyExtra2Price=price;
+    }
+    public double itemizeHamburger() {
+        System.out.println( "Healthy hamburger on a Brown rye roll with "+super.getMeat()+", price is "+super.getPrice());
+        if(healthyExtra1Name!=null)
+            System.out.println( "Added "+healthyExtra1Name+" for an extra "+healthyExtra1Price);
+        if(healthyExtra2Name!=null)
+            System.out.println( "Added "+healthyExtra2Name+" for an extra "+healthyExtra2Price);
+        System.out.println( "Total Healthy Burger price is  "+super.getPrice());
+        return super.getPrice() +healthyExtra1Price+healthyExtra2Price;
+
+    }
 }
