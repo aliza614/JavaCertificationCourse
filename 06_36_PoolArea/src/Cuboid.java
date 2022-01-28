556ed954ba55a0d6eb0c31bbe2f42d64f44cddfa
Aliza <alizaoconnor1@yahoo.com>
2022-01-27 17:08:10 -0700

    solution

---
+++ 06_36_PoolArea/src/Cuboid.java
@@ -0,0 +1,16 @@
+public class Cuboid extends Rectangle{
+    private double height;
+
+    public Cuboid(double width, double length, double height) {
+        super(width, length);
+        this.height = Math.max(0.0,height);
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public double getVolume() {
+        return height*this.getArea();
+    }
+}
