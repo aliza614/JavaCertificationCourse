ff9837159a0caa1c1ea1d8f66de12d2a7e3ea590
Aliza <alizaoconnor1@yahoo.com>
2022-02-22 19:48:24 -0700

    USE REENTRANT LOCK

--- 15_288_Challenge1/src/BankAccount.java
+++ 15_288_Challenge1/src/BankAccount.java
@@ -1,19 +1,38 @@
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class BankAccount {
     private double balance;
     private String accountNumber;
-
+    /*solution 3*/
+    private Lock lock;
     public BankAccount(double balance, String accountNumber) {
         this.balance = balance;
         this.accountNumber = accountNumber;
+        this.lock=new ReentrantLock();
     }
     public /*synchronized IS 1 SOLUTION*/void deposit(double amount){
-        /*THIS IS A SECOND SOLUTION*/synchronized (this) {
+        /*THIS IS A SECOND SOLUTION synchronized (this) {
             balance += amount;
+        }*/
+        /*THIS IS THE THIRD SOLUTION*/
+        lock.lock();
+        try{
+            balance+=amount;
+        }finally{
+            lock.unlock();
         }
     }
     public /*synchronized IS 1 SOLUTION*/void withdraw(double amount){
-        /*THIS IS A SECOND SOLUTION*/synchronized (this){
+        /*THIS IS A SECOND SOLUTION synchronized (this){
             balance-=amount;
+        }*/
+        /*THIS IS THE THIRD SOLUTION*/
+        lock.lock();
+        try{
+            balance-=amount;
+        }finally {
+            lock.unlock();
         }
     }
 
