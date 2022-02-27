44d8f970840de3bc2b3c7bae37e4f0473d56cb59
Aliza <alizaoconnor1@yahoo.com>
2022-02-27 15:44:13 -0700

    committing with test2 being the correct one

---
+++ 18_318_unitTestingWithJUnit_Banking/src/BankAccount.java
@@ -0,0 +1,32 @@
+public class BankAccount {
+    private String firstName;
+    private String lastName;
+    private double balance;
+
+    public BankAccount(String firstName, String lastName, double balance) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.balance = balance;
+    }
+    //The branch arguement is true if a customer is performing the transaction
+    // at a branch with a teller.
+    //it's false if the customer is performing the transaction at an ATM
+    public double deposit(double amount, boolean branch){
+        balance +=amount;
+        return balance;
+    }
+
+    //The branch arguement is true if a customer is performing the transaction
+    // at a branch with a teller.
+    //it's false if the customer is performing the transaction at an ATM
+
+    public double withdraw(double amount, boolean branch){
+        balance-=amount;
+        return balance;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+    //more methods that use first name and last name and perform other functions
+}
---
+++ 18_318_unitTestingWithJUnit_Banking/src/BankAccountTest.java
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.*;
+
+public class BankAccountTest {
+
+    @Test
+    public void deposit() {
+    }
+
+    @org.junit.Test
+    public void withdraw() {
+    }
+
+    @org.junit.Test
+    public void getBalance() {
+    }
+}
