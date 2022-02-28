c13994f657982d28ab89de740c60c71dcee7f876
Aliza <alizaoconnor1@yahoo.com>
2022-02-28 14:20:55 -0700

    challenge 1

---
+++ 18_322_JunitChallenge/src/Utilities.java
@@ -0,0 +1,42 @@
+public class Utilities {
+    //returns a char array containing every nth char.  When'
+    // sourceArray.length<n, returns sourceArray
+    public char[] everyNthChar(char[] sourceArray, int n){
+        if (sourceArray==null||sourceArray.length<n)
+            return sourceArray;
+        int returnedLength=sourceArray.length/n;
+        char[] result=new char[returnedLength];
+        int index=0;
+
+        for (int i = n-1; i < sourceArray.length ; i++) {
+            result[index++]=sourceArray[i];
+
+        }
+        return result;
+    }
+    //"ABBCDEEF"->"ABCDEF"
+    //"ABCBDEEF"->"ABCBDEF" the 2 bs aren't next to eachother so aren't removed
+    public String removePairs(String source){
+        if (source.length()<2) return source;
+        StringBuilder sb=new StringBuilder();
+        char[] string=source.toCharArray();
+        for (int i = 0; i < string.length-1; i++) {
+            if (string[i]==string[i+1])
+                sb.append(string[i++]);
+            else
+                sb.append(string[i]);
+        }
+        return sb.toString();
+    }
+    public int converter(int a, int b){
+        return a/b+(a*30)-2;
+    }
+    public String nullIfOddLength(String source){
+        if(source.length()%2==0)
+        {
+            return source;
+        }else{
+            return null;
+        }
+    }
+}
---
+++ 18_322_JunitChallenge/src/UtilitiesTest.java
@@ -0,0 +1,36 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UtilitiesTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void everyNthChar() {
+        fail("this is a dummy test");
+    }
+
+    @Test
+    void removePairs() {
+        fail("this is a dummy test");
+    }
+
+    @Test
+    void converter() {
+        fail("this is a dummy test");
+    }
+
+    @Test
+    void nullIfOddLength() {
+        fail("this is a dummy test");
+    }
+}
