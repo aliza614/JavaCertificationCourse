156742fe24aae253b52c23f509756a2e4cf517a8
Aliza <alizaoconnor1@yahoo.com>
2022-02-28 14:20:39 -0700

    committing with test2 being the correct one

--- 18_318_unitTestingWithJUnit_Banking/src/BankAccount.java
+++ 18_318_unitTestingWithJUnit_Banking/src/BankAccount.java
@@ -3,11 +3,16 @@ public class BankAccount {
     private String lastName;
     private double balance;
 
+    public static final int CHECKING=1;
+    public static final int SAVINGS=2;
 
-    public BankAccount(String firstName, String lastName, double balance) {
+    private int accountType;
+
+    public BankAccount(String firstName, String lastName, double balance, int typeOfAccount) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.balance = balance;
+        this.accountType=typeOfAccount;
     }
     //The branch arguement is true if a customer is performing the transaction
     // at a branch with a teller.
@@ -22,6 +27,9 @@ public class BankAccount {
     //it's false if the customer is performing the transaction at an ATM
 
     public double withdraw(double amount, boolean branch){
+        if (amount>500 && !branch){
+            throw new IllegalArgumentException();
+        }
         balance-=amount;
         return balance;
     }
@@ -29,5 +37,9 @@ public class BankAccount {
     public double getBalance() {
         return balance;
     }
+    public boolean isChecking(){
+        return accountType==CHECKING;
+    }
+
     //more methods that use first name and last name and perform other functions
 }
--- 18_318_unitTestingWithJUnit_Banking/src/BankAccountTest2.java
+++ 18_318_unitTestingWithJUnit_Banking/src/BankAccountTest2.java
@@ -1,30 +1,56 @@
-import org.junit.jupiter.api.Test;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class BankAccountTest2 {
+    private BankAccount bankAccount;
+    private static int count=0;
+    @BeforeAll
+    public static void beforeClass(){
+//        int count=0;
+        System.out.println("This executes before any test cases.  Count="+count);
+    }
 
+    @BeforeEach
+    public void setup(){
+        bankAccount=new BankAccount("Tim", "Buchalka", 1000.00, BankAccount.CHECKING);
+        System.out.println("Running a test...");
+    }
     @Test
     void deposit() {
         //fail("This test has yet to be implemented");
-        BankAccount bankAccount=new BankAccount("Tim", "Buchalka", 1000.00);
+//        BankAccount bankAccount=new BankAccount("Tim", "Buchalka", 1000.00, BankAccount.CHECKING);
         double balance=bankAccount.deposit(200.00,true);
         assertEquals(1200.00, balance, 0);
         assertEquals(1200.00, bankAccount.getBalance(),0);
     }
 
     @Test
-    void withdraw() {
+    void withdraw_branch() {
         //fail("This test has yet to be implemented");
-        BankAccount bankAccount=new BankAccount("Tim", "Buchalka", 1000.00);
-        double balance=bankAccount.withdraw(200.00,true);
-        assertEquals(800.00, balance, 0);;
+//        BankAccount bankAccount=new BankAccount("Tim", "Buchalka", 1000.00, BankAccount.CHECKING);
+        double balance=bankAccount.withdraw(600.00,true);
+        assertEquals(400.00, balance, 0);;
+    }
+    @Test //JUNIT4 way (expected =IllegalArgumentException.class)
+    void withdraw_notBranch() {
+        //fail("This test has yet to be implemented");
+//        BankAccount bankAccount=new BankAccount("Tim", "Buchalka", 1000.00, BankAccount.CHECKING);
+        Exception exception=assertThrows(IllegalArgumentException.class,()-> {
+            double balance=bankAccount.withdraw(600.00,false);
+        });
+        assertTrue(exception.getClass().equals(IllegalArgumentException.class));
+
     }
 
     @Test
     void getBalance_deposit() {
         //fail("This test has yet to be implemented");
-        BankAccount bankAccount=new BankAccount("Tim", "Buchalka", 1000.00);
+//        BankAccount bankAccount=new BankAccount("Tim", "Buchalka", 1000.00, BankAccount.CHECKING);
         double balance=bankAccount.deposit(200.00,true);
         assertEquals(1200.00, bankAccount.getBalance(),0);
     }
@@ -32,9 +58,24 @@ class BankAccountTest2 {
     @Test
     void getBalance_withdraw() {
         //fail("This test has yet to be implemented");
-        BankAccount bankAccount=new BankAccount("Tim", "Buchalka", 1000.00);
+//        BankAccount bankAccount=new BankAccount("Tim", "Buchalka", 1000.00, BankAccount.CHECKING);
         double balance=bankAccount.withdraw(200.00,true);
         assertEquals(800.00, bankAccount.getBalance(),0);
+    }
+
+    @Test
+    void isChecking_true(){
+//        BankAccount bankAccount=new BankAccount("Tim", "Buchalka", 1000.00, BankAccount.CHECKING);
+        assertTrue(bankAccount.isChecking(), "the account is not a checking account");
+    }
+    @AfterAll
+    public static void afterClass(){
+        System.out.println("This executes after any tests.  Count="+ count++);
+    }
+
+    @AfterEach
+    public void teardown(){
+        System.out.println("Count= "+ count++);
     }
 
 }
